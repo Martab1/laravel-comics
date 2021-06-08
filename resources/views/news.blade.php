931b368f9a8b69634939a36ccdd9b0a2660028c8
Martab1 <martabernardo86@gmail.com>
2021-06-09 00:04:16 +0200

    .

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -8,7 +8,7 @@
 <section class="container">
       
     <div class="box">
-        <h2>news </h2>
+        <h2 class="text-center">news </h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti rerum sed blanditiis molestias odio nam delectus, necessitatibus ratione recusandae sit at consequuntur perferendis quasi totam? Fugiat sunt voluptate beatae? Ea.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti rerum sed blanditiis molestias odio nam delectus, necessitatibus ratione recusandae sit at consequuntur perferendis quasi totam? Fugiat sunt voluptate beatae? Ea.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti rerum sed blanditiis molestias odio nam delectus, necessitatibus ratione recusandae sit at consequuntur perferendis quasi totam? Fugiat sunt voluptate beatae? Ea.</p>
